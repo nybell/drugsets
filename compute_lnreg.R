d63e1b3234e5998bddf1db1606d39599eff9f7a6
Nathaniel Bell <55631896+nybell@users.noreply.github.com>
2022-05-25 09:34:48 +0200

    removed rrtable from compute_lnreg.R

--- compute_lnreg.R
+++ compute_lnreg.R
@@ -10,7 +10,6 @@ args = commandArgs(trailingOnly = TRUE)  # setcorrs.rdata  (1), metadata (2), gr
 # library
 library(tidyr)
 library(dplyr)
-library(rrtable)
 
 # load data from compute_corrs.r
 load(args[1])
