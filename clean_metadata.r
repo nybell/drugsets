4133b1fb2c04f158134232efc1866ddd71a2f779
Nathaniel Bell <55631896+nybell@users.noreply.github.com>
2022-05-31 15:05:10 +0200

    updated clean_metadata.r and metadata.rdata file to have moa and ind group names have spaces in them for better readability

--- clean_metadata.r
+++ clean_metadata.r
@@ -16,27 +16,27 @@ meta <- read.csv('~/drugsets/setcorrs_dg/geneset_meta.csv', na.strings=c("","NA"
 colnames(meta)[9] <- 'atc'
 
 # clean data 
-meta$indication <- lapply(meta$indication, function(x) tolower(gsub("[^[:alnum:],]", "", x)))     # meta$indication
-meta$moa <- lapply(meta$moa, function(x) tolower(gsub("[^[:alnum:],]", "", x)))                   # meta$moa
-meta$atc <- lapply(meta$atc, function(x) (gsub("[^[:alnum:],]", "", x)))                          # meta$atc
+meta$indication <- lapply(meta$indication, function(x) tolower(gsub("[^[:alnum:][:space:],]", "", x)))     # meta$indication
+meta$moa <- lapply(meta$moa, function(x) tolower(gsub("[^[:alnum:][:space:],]", "", x)))                   # meta$moa
+meta$atc <- lapply(meta$atc, function(x) (gsub("[^[:alnum:][:space:],]", "", x)))                          # meta$atc
 meta$disease_area <- lapply(meta$disease_area, function(x) tolower(gsub("[^[:alnum:],]", "", x))) # meta$disease_area
 meta$ENTREZ <- lapply(meta$ENTREZ, function(x) gsub("[^[:alnum:],]", "", x))                      # meta$ENTREZ
 meta$target <- lapply(meta$target, function(x) gsub("[^[:alnum:],]", "", x))                      # meta$target
 
 # get atc codes
-tmp <- meta %>% separate_rows(atc, sep = ',')
+tmp <- meta %>% separate_rows(atc, sep = ',  ')
 tmp <- subset(tmp, is.na(atc) == FALSE & atc != "")
 codesAtc <- unique(tmp$atc)
 rm(tmp)
 
 # get clinical indication codes
-tmp <- meta %>% separate_rows(indication, sep = ',')
+tmp <- meta %>% separate_rows(indication, sep = ', ')
 tmp <- subset(tmp, is.na(indication) == FALSE & indication != "")
 codesInd <- unique(tmp$indication)
 rm(tmp)
 
 # get moa codes
-tmp <- meta %>% separate_rows(moa, sep = ',')
+tmp <- meta %>% separate_rows(moa, sep = ', ')
 tmp <- subset(tmp, is.na(moa) == FALSE & moa != "")
 codesMoa <- unique(tmp$moa)
 rm(tmp)
@@ -60,9 +60,9 @@ write.csv(ind_data, 'ind_groups.csv', row.names = FALSE)
 
 
 # split strings into vectors
-meta$indication <- lapply(meta$indication, function(x) as.vector(strsplit(x, ',')[[1]]))     # meta$indication
-meta$moa <- lapply(meta$moa, function(x) as.vector(strsplit(x, ',')[[1]]))                   # meta$moa
-meta$atc <- lapply(meta$atc, function(x) as.vector(strsplit(x, ',')[[1]]))                   # meta$atc
+meta$indication <- lapply(meta$indication, function(x) as.vector(strsplit(x, ', ')[[1]]))     # meta$indication
+meta$moa <- lapply(meta$moa, function(x) as.vector(strsplit(x, ', ')[[1]]))                   # meta$moa
+meta$atc <- lapply(meta$atc, function(x) as.vector(strsplit(x, ',  ')[[1]]))                   # meta$atc
 meta$disease_area <- lapply(meta$disease_area, function(x) as.vector(strsplit(x, ',')[[1]])) # meta$disease_area
 meta$ENTREZ <- lapply(meta$ENTREZ, function(x) as.vector(strsplit(x, ',')[[1]]))             # meta$ENTREZ
 meta$target <- lapply(meta$target, function(x) as.vector(strsplit(x, ',')[[1]]))             # meta$indication
